5c0406fc05e13ea1a845df74decdf9a342b0d9fa
dune6 <ilya.voznesensky@gmail.com>
2025-03-10 17:06:09 +0300

    init proj

--- internal/database/database.go
+++ internal/database/database.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	_ "github.com/joho/godotenv/autoload"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -21,18 +22,41 @@ type service struct {
 }
 
 var (
-	host = os.Getenv("BLUEPRINT_DB_HOST")
-	port = os.Getenv("BLUEPRINT_DB_PORT")
-	//database = os.Getenv("BLUEPRINT_DB_DATABASE")
+	appName    = os.Getenv("APP_NAME")
+	dbUserName = os.Getenv("DB_USERNAME")
+	dbPassword = os.Getenv("DB_USER_PASSWORD")
+	dbHost     = os.Getenv("DB_HOST")
 )
 
 func New() Service {
-	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(fmt.Sprintf("mongodb://%s:%s", host, port)))
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	log.Println(appName, dbUserName, dbPassword, dbHost)
+	uri := fmt.Sprintf(
+		"mongodb+srv://%s:%s@%s/?retryWrites=true&w=majority&appName=%s",
+		dbUserName,
+		dbPassword,
+		dbHost,
+		appName,
+	)
+	log.Println(uri)
+	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
+
+	client, err := mongo.Connect(
+		context.Background(),
+		opts,
+	)
+
+	defer func() {
+		if err = client.Disconnect(context.Background()); err != nil {
+			log.Fatal("failed to disconnect from mongodb: ", err)
+		}
+	}()
 
 	if err != nil {
 		log.Fatal(err)
 
 	}
+	
 	return &service{
 		db: client,
 	}
@@ -42,10 +66,12 @@ func (s *service) Health() map[string]string {
 	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
 	defer cancel()
 
-	err := s.db.Ping(ctx, nil)
-	if err != nil {
+	// Send a ping to confirm a successful connection
+	if err := s.db.Database("admin").RunCommand(
+		ctx, bson.D{{"ping", 1}}).Err(); err != nil {
 		log.Fatalf("db down: %v", err)
 	}
+	log.Println("Pinged your deployment. You successfully connected to MongoDB!")
 
 	return map[string]string{
 		"message": "It's healthy",
